ba6b503a96140166c35e83aba32c57336afc4ea0
shohruza2024 <salibayevaruxshona1312@gmail.com>
2026-01-20 08:31:47 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,19 @@
+# 1
+s = "0123456789"
+print(s[2:9:2])
+
+# 2
+lst = ['a','b','c','d','e','f','g']
+print(lst[::-2])
+
+# 3
+s = "machinelearning"
+print(s[:7], s[7:])
+
+# 4
+s = "unbelievable"
+print(s[2:-4])
+
+# 5
+s = "abcdefgh"
+print(s[1::2][::-1] + s[::2][::-1])
